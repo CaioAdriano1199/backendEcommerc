85d65245fbc0e211d1e9dab601f98f8c62f47e6a
CaioAdriano1199 <caioadriano1199@gmail.com>
2024-12-03 21:10:25 -0300

    com barra de pesquisa

--- src/main/java/org/senac/aula01/model/Produto.java
+++ src/main/java/org/senac/aula01/model/Produto.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//isso é um comentario
+
 @Entity
 public class Produto {
     @Id
